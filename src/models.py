2c189b379954d1b2f7281df8e04b8f7e4d17e4e7
Finnvoid <neku113@gmail.com>
2020-07-22 13:45:00 -0400

    Troubleshooting Heroku #2

--- src/models.py
+++ src/models.py
@@ -28,12 +28,14 @@ def setup_db(app, database_path=database_path):
     db.app = app
     moment.app = app
     db.init_app(app)
+    db.drop_all()
     db.create_all()
 
-
+'''
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+'''
 
 
 
